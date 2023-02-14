e624fe89c73655d05721703386a22b9c820e905a
umer6921 <umerm6921@gmail.com>
2023-02-14 16:06:34 +0500

    main

---
+++ assets/js/carousel.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    items:1,
+    margin: 10,
+
+    responsive: {
+      0: {
+        items: 1
+      },
+
+    }
+  })
+
+
+
+});
